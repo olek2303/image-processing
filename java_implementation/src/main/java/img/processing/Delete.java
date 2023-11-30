a8aa0803eafcb3b28ba5bb42a968910548f7663f
olek2303 <olek2303@gmail.com>
2023-11-30 19:15:07 +0100

    fixing bugs with multipied files

---
+++ java_implementation/src/main/java/img/processing/Delete.java
@@ -0,0 +1,25 @@
+package java_implementation.src.main.java.img.processing;
+
+public class Delete {
+    public static int[] deleteArea(int[] imageData, int width, int height, int x, int y, int areaWidth, int areaHeight, int channels) {
+        // Checking if area is possible to delete
+        if (x < 0 || y < 0 || x + areaWidth > width || y + areaHeight > height) {
+            System.err.println("The area extends beyond the image size.");
+            return imageData;
+        }
+
+        int[] newImageData = imageData.clone();
+
+        // Deleting area
+        for (int i = y; i < y + areaHeight; i++) {
+            for (int j = x; j < x + areaWidth; j++) {
+                int index = (i * width + j) * channels;
+                for (int c = 0; c < channels; c++) {
+                    newImageData[index + c] = 255;
+                }
+            }
+        }
+
+        return newImageData;
+    }
+}
--- python_implementation/merge.py
+++ python_implementation/merge.py
@@ -1,40 +1,36 @@
 from image import ImageRead
 from PIL import Image
 
-def blend_colors(rgb1, rgb2, alpha):
-    r1, g1, b1 = (rgb1 >> 16) & 0xFF, (rgb1 >> 8) & 0xFF, rgb1 & 0xFF
-    r2, g2, b2 = (rgb2 >> 16) & 0xFF, (rgb2 >> 8) & 0xFF, rgb2 & 0xFF
-
-    blendedR = int((r1 * (1.0 - alpha)) + (r2 * alpha))
-    blendedG = int((g1 * (1.0 - alpha)) + (g2 * alpha))
-    blendedB = int((b1 * (1.0 - alpha)) + (b2 * alpha))
-
-    return (blendedR << 16) | (blendedG << 8) | blendedB
-
 def mergeImages(im1, im2, alpha):
     mergedW = max(im1.img.width, im2.img.width)
     mergedH = max(im1.img.height, im2.img.height)
 
-    merged = Image.new("RGB", (mergedW, mergedH))
+    merged = ImageRead()
+    merged.filename = "images.jpg"
+    merged.img = Image.new("RGB", (mergedW, mergedH))
 
-    size1 = im1.img.width * im1.img.height * 3
-    size2 = im2.img.width * im2.img.height * 3
+    size1 = im1.img.width * im1.img.height 
+    size2 = im2.img.width * im2.img.height 
 
-    if size1 > size2: 
-        merged = im1.img
-        for y in range(im2.img.height):
-            for x in range(im2.img.width):
-                rgb1 = merged.getpixel((x, y))
-                rgb2 = im2.img.getpixel((x, y))
-                blended_rgb = blend_colors(rgb1, rgb2, alpha)
-                merged.img.putpixel((x, y), blended_rgb)
-    else:
-        merged = im2.img
+    if size1 > size2:
         for y in range(im1.img.height):
             for x in range(im1.img.width):
-                rgb1 = merged.getpixel((x, y))
-                rgb2 = im1.img.getpixel((x, y))
-                blended_rgb = blend_colors(rgb1, rgb2, alpha)
-                merged.img.putpixel((x, y), blended_rgb)
-        
+                merged.img.putpixel((x, y), tuple(im1.img.getpixel((x, y))))
+    else:
+        for y in range(im2.img.height):
+            for x in range(im2.img.width):
+                merged.img.putpixel((x, y), tuple(im2.img.getpixel((x, y))))
+
+    for y in range(mergedH):
+        for x in range(mergedW):
+            p1 = im1.img.getpixel((x, y))
+            p2 = im2.img.getpixel((x, y))
+            p1 = tuple(map(int, p1))
+            p2 = tuple(map(int, p2))
+            merged_pixel_value = tuple (
+                int(p1_channel * (1.0 - float(alpha)) + p2_channel * float(alpha))
+                for p1_channel, p2_channel in zip(p1, p2)
+            )
+            merged.img.putpixel((x, y), merged_pixel_value)
+                
     return merged;
