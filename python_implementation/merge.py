412f2a2279382976e1b73da5a68f838eb3d8fe15
olek2303 <olek2303@gmail.com>
2023-12-11 14:04:36 +0100

    repaired merging function for python

--- python_implementation/merge.py
+++ python_implementation/merge.py
@@ -9,28 +9,19 @@ def mergeImages(im1, im2, alpha):
     merged.filename = "images.jpg"
     merged.img = Image.new("RGB", (mergedW, mergedH))
 
-    size1 = im1.img.width * im1.img.height 
-    size2 = im2.img.width * im2.img.height 
-
-    if size1 > size2:
-        for y in range(im1.img.height):
-            for x in range(im1.img.width):
-                merged.img.putpixel((x, y), tuple(im1.img.getpixel((x, y))))
-    else:
-        for y in range(im2.img.height):
-            for x in range(im2.img.width):
-                merged.img.putpixel((x, y), tuple(im2.img.getpixel((x, y))))
-
     for y in range(mergedH):
         for x in range(mergedW):
-            p1 = im1.img.getpixel((x, y))
-            p2 = im2.img.getpixel((x, y))
+            p1 = im1.img.getpixel((x * im1.img.width // mergedW, y * im1.img.height // mergedH))
+            p2 = im2.img.getpixel((x * im2.img.width // mergedW, y * im2.img.height // mergedH))
+            
             p1 = tuple(map(int, p1))
             p2 = tuple(map(int, p2))
-            merged_pixel_value = tuple (
+            
+            merged_pixel_value = tuple(
                 int(p1_channel * (1.0 - float(alpha)) + p2_channel * float(alpha))
                 for p1_channel, p2_channel in zip(p1, p2)
             )
+            
             merged.img.putpixel((x, y), merged_pixel_value)
                 
     return merged;
